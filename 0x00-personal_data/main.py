b17726ec34887a170578c33622f622344c03e503
habiibullah <ajiboyhabiib@gmail.com>
2023-08-03 06:17:02 +0000

    Encrypting passwords

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -36,3 +36,13 @@ for row in cursor:
 cursor.close()
 db.close()
 
+hash_password = __import__('encrypt_password').hash_password
+
+password = "MyAmazingPassw0rd"
+print(hash_password(password))
+print(hash_password(password))
+
+is_valid = __import__('encrypt_password').is_valid
+encrypted_password = hash_password(password)
+print(encrypted_password)
+print(is_valid(encrypted_password, password))
