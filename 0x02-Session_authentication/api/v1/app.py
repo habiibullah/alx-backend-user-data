161f91b1d97868d69525ea0d6640afd627d78d81
habiibullah <ajiboyhabiib@gmail.com>
2023-08-10 07:14:47 -0700

    fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -24,6 +24,7 @@ if auth_type == 'session_auth':
     auth = SessionAuth()
 
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """Not found handler.
@@ -54,20 +55,16 @@ def authenticate_user():
             "/api/v1/status/",
             "/api/v1/unauthorized/",
             "/api/v1/forbidden/",
-            "/api/v1/auth_session/login/"
+            "/api/v1/auth_session/login/",
         ]
-
-    if not(auth.require_auth(request.path, excluded_paths)):
-        return
-    if (auth.authorization_header(request)) is None\
-       and auth.session_cookie(request) is None:
-        abort(401)
-
-    current_user = auth.current_user(request)
-    if current_user is None:
-        abort(403)
-
-    request.current_user = current_user
+        if auth.require_auth(request.path, excluded_paths):
+            user = auth.current_user(request)
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
+                abort(401)
+            if user is None:
+                abort(403)
+            request.current_user = user
 
 
 
