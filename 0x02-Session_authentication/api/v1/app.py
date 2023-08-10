91d0cb86cf7ae0412855555037e09cd3a096e040
habiibullah <ajiboyhabiib@gmail.com>
2023-08-10 07:19:10 -0700

    fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,7 +9,6 @@ from flask_cors import (CORS, cross_origin)
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.session_auth import SessionAuth 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
