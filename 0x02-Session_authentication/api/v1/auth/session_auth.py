f6dfe327cf9e56847cb81436b824b6ca009df0d0
habiibullah <ajiboyhabiib@gmail.com>
2023-08-10 10:00:07 -0700

    fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
+from uuid import uuid4
+from flask import request
+from models.user import User
 from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
-    pass
+    """Session authentication class.
+    """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session id for the user.
+        """
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+
