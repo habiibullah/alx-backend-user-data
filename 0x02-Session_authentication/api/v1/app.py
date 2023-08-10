f3734831b72f5a16421afc1e8c0d753777671b4a
habiibullah <ajiboyhabiib@gmail.com>
2023-08-10 09:54:03 -0700

    fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,23 +7,27 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 
 from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_auth import SessionAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
+
 if auth_type == 'auth':
+
     auth = Auth()
-from api.v1.auth.auth import Auth
 
 if auth_type == 'basic_auth':
+
     auth = BasicAuth()
-from api.v1.auth.basic_auth import BasicAuth
 
 if auth_type == 'session_auth':
+
     auth = SessionAuth()
-from api.v1.auth.session_auth import SessionAuth
 
 @app.errorhandler(404)
 def not_found(error) -> str:
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from .auth import Auth
+
+from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
     pass
