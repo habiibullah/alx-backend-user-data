f0424b6019278321cb19b74b481010757ba13756
habiibullah <ajiboyhabiib@gmail.com>
2023-08-13 04:13:52 -0700

    Session Authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -10,7 +10,7 @@ from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.basic_auth import BasicAuth
-
+from api.v1.auth.session_exp_auth import SessionExpAuth
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -28,6 +28,10 @@ if auth_type == 'basic_auth':
 if auth_type == 'session_auth':
 
     auth = SessionAuth()
+
+if auth_type == 'session_exp_auth':
+
+    auth = SessionExpAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
